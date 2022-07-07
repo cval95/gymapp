c8ba9222b2578dce0d3b31ef04c079d5f72bdf6e
cval95 <cormacvalentine@outlook.com>
2022-07-07 17:35:20 +0100

    commit

---
+++ backend/models/routineModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const routineSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    workouts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Workout"
+    }],
+    exercises: [{
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+    }]
+});
+
+
+
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+   
+    exercises: [{
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+    }]
+});
+
+const exerciseSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Workout', workoutSchema)
+module.exports = mongoose.model('Excerise', exerciseSchema)
+module.exports = mongoose.model('Routine', routineSchema)
+
