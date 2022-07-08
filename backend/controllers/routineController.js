0c336586c5850ba1a3f2a29c0487ef72b5e83eb3
cval95 <cormacvalentine@outlook.com>
2022-07-08 21:29:12 +0100

    commit

---
+++ backend/controllers/routineController.js
@@ -0,0 +1,111 @@
+const Routine = require('../models/workoutModel')
+const mongoose = require('mongoose')
+
+//get all workouts
+const getRoutines = async(req, res) =>{
+    const routines = await Routine.find({}).sort({createdAt: -1})
+
+    res.status(200).json(routines)
+}
+
+
+
+
+
+//get a single workout 
+const getRoutine = async (req, res) => {
+    const { id } = req.params
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({error: 'No such routine'})
+    }
+  
+    const routine = await Routine.findById(id)
+  
+    if (!workout) {
+      return res.status(404).json({error: 'No such routine'})
+    }
+  
+    res.status(200).json(routine)
+  }
+  
+
+
+// create a new workout
+const createRoutine = async (req, res) => {
+  const {title, load, reps} = req.body
+
+  let emptyFields = []
+
+  if (!title) {
+    emptyFields.push('title')
+  }
+  if (!load) {
+    emptyFields.push('load')
+  }
+  if (!reps) {
+    emptyFields.push('reps')
+  }
+  if (emptyFields.length > 0) {
+    return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+  }
+
+  // add to the database
+  try {
+    const routine = await Routine.create({ title, load, reps })
+    res.status(200).json(routine)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+///delete a routine
+const deleteRoutine = async (req, res) => {
+    const { id } = req.params
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({error: 'No such routine'})
+    }
+  
+    const routine = await Routine.findOneAndDelete({_id: id})
+  
+    if(!routine) {
+      return res.status(400).json({error: 'No such workout'})
+    }
+  
+    res.status(200).json(routine)
+  }
+
+
+//update routine
+const updateRoutine = async (req, res) => {
+    const { id } = req.params
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({error: 'No such routine'})
+    }
+  
+    const routine = await Routine.findOneAndUpdate({_id: id}, {
+      ...req.body
+    })
+  
+    if (!routine) {
+      return res.status(400).json({error: 'No such routine'})
+    }
+  
+    res.status(200).json(routine)
+  }
+  
+
+
+
+
+
+module.exports = {
+    getRoutine,
+    getRoutine,
+    createRoutine,
+    deleteRoutine,
+    updateRoutine
+
+  }
---
+++ backend/routes/routines.js
@@ -0,0 +1,30 @@
+
+
+const express = require('express')
+const {
+  getRoutines, 
+  getRoutine, 
+  createRoutine, 
+  deleteRoutine, 
+  updateRoutine
+ 
+} = require('../controllers/RoutineController')
+
+const router = express.Router()
+
+// GET all Routines
+router.get('/', getRoutines)
+
+// GET a single Routine
+router.get('/:id', getRoutine)
+
+// POST a new Routine
+router.post('/', createRoutine)
+
+// DELETE a Routine
+router.delete('/:id', deleteRoutine)
+
+// UPDATE a Routine
+router.patch('/:id', updateRoutine)
+
+module.exports = router
