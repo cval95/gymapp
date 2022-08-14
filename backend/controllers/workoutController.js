74eed72c356314c730158c8b016fc6baecac5666
cval95 <cormacvalentine@outlook.com>
2022-08-14 20:36:28 +0100

    commit

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,33 +1,33 @@
 const Workout = require('../models/workoutModel')
 const mongoose = require('mongoose')
+// get all workouts
+const getWorkouts = async (req, res) => {
+  const user_id = req.user._id
 
-//get all workouts
-const getWorkouts = async(req, res) =>{
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+  const workouts = await Workout.find({user_id}).sort({createdAt: -1})
 
-    res.status(200).json(workouts)
+  res.status(200).json(workouts)
 }
 
 
 
 
-
-//get a single workout 
+// get a single workout
 const getWorkout = async (req, res) => {
-    const { id } = req.params
-  
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({error: 'No such workout'})
-    }
-  
-    const workout = await Workout.findById(id)
-  
-    if (!workout) {
-      return res.status(404).json({error: 'No such workout'})
-    }
-  
-    res.status(200).json(workout)
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
   }
+
+  const workout = await Workout.findById(id)
+
+  if (!workout) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+  
+  res.status(200).json(workout)
+}
   
 
 
@@ -50,14 +50,16 @@ const createWorkout = async (req, res) => {
     return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
   }
 
-  // add to the database
-  try {
-    const workout = await Workout.create({ title, load, reps })
-    res.status(200).json(workout)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
+ // add doc to db
+ try {
+  const user_id = req.user._id
+  const workout = await Workout.create({title, load, reps, user_id})
+  res.status(200).json(workout)
+} catch (error) {
+  res.status(400).json({error: error.message})
 }
+}
+
 
 ///delete a workout
 const deleteWorkout = async (req, res) => {
